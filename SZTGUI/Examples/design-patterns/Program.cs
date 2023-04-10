6912c84b4a25952b44cd46445fad2824178c9e48
Balázs Tóth <grandmastar27@gmail.com>
2023-04-10 11:58:26 +0200

    Added facade code example

---
+++ SZTGUI/Examples/design-patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Structural();
+    }
+
+    static void Structural()
+    {
+        System.Console.WriteLine("Structural");
+        System.Console.WriteLine("[-] Facade");
+        Facade();
+    }
+
+    static void Facade()
+    {
+        Subsystem1 subsystem1 = new Subsystem1();
+        Subsystem2 subsystem2 = new Subsystem2();
+        Facade facade = new Facade(subsystem1, subsystem2);
+        Client.ClientCode(facade);
+    }
+}
---
+++ SZTGUI/Examples/design-patterns/Structural/Facade.cs
@@ -0,0 +1,46 @@
+﻿namespace DesignPatterns
+{
+    public class Facade
+    {
+        protected Subsystem1 _subsystem1;
+        protected Subsystem2 _subsystem2;
+
+        public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
+        {
+            this._subsystem1 = subsystem1;
+            this._subsystem2 = subsystem2;
+        }
+
+        public string Operation()
+        {
+            string result = "Facade initializes subsystems:\n";
+            result += this._subsystem1.Operation1;
+            result += this._subsystem2.Operation1;
+            result += "Facade orders subsystems to perform the action:\n";
+            result += this._subsystem1.OperationN;
+            result += this._subsystem2.OperationZ;
+
+            return result;
+        }
+    }
+
+    public class Subsystem1
+    {
+        public string Operation1 => "Subsystem1: Ready!\n";
+        public string OperationN => "Subsystem1: Go!\n";
+    }
+
+    public class Subsystem2
+    {
+        public string Operation1 => "Subsystem2: Get ready!\n";
+        public string OperationZ => "Subsystem2: Fire!\n";
+    }
+
+    class Client
+    {
+        public static void ClientCode(Facade facade)
+        {
+            System.Console.Write(facade.Operation());
+        }
+    }
+}
